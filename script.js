6cf87f100d6012b6737bfbe5b45bca6e0911fee8
Yaroslav-Shkrabaliuk <137926669+Yaroslav-Shkrabaliuk@users.noreply.github.com>
2023-06-29 00:04:28 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict"
+// console.log(document.body.firstElementChild);
+// console.log(document.body.children[1]);
+// console.log(document.body.children[1].lastElementChild);
+
+
+
+// Як знайти?…
+
+// Таблиця з id="age-table".
+// Усі елементи label всередині цієї таблиці (їх має бути 3).
+// Перший td у цій таблиці (зі словом “Age”).
+// form з name="search".
+// Перший input у цій формі.
+// Останній input у цій формі.
